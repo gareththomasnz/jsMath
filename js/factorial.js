eecac390ffc6966b6a79040f5df56ed11194866e
gareththomasnz <gareththomasnz@yahoo.com>
2016-09-05 18:52:49 +1200

    fourth commit

--- js/factorial.js
+++ js/factorial.js
@@ -5,7 +5,7 @@ var x = 6;
 $(document).ready(function(){
         console.log(x);
         $('#submitButton').click(function(){
-                f = 1;
+                f = 1;         
                 x = parseFloat($("#input1").val());
                 if(x>0){
                         while(x>1){
@@ -13,8 +13,8 @@ $(document).ready(function(){
                         x = x - 1;                      
                 }
                 console.log(f);
-                $('#details').html('The factorial is ' + f);
-                }else{
+                $('#details').html('<b>The factorial is ' + f + '</b>');
+                }else if(x<=0){
                         alert('x must be greater than zero');
                 }
                 
---
+++ js/temperature.js
@@ -0,0 +1,35 @@
+"use strict";
+var C = 0;
+var F = 0;
+var startbound = 32;
+var endbound = 100;
+var increment = 3;
+var htmlout = "";
+$(document).ready(function() {
+	// function code goes here
+	function getC(F) {
+		return (F - 32) * 5 / 9;
+	}
+	$('#submitButton').click(function() {
+		// JavaScript code here
+		startbound = parseFloat($('#input1').val());
+		endbound = parseFloat($('#input2').val());
+		increment = parseFloat($('#increment').val());
+		console.log(startbound);
+		console.log(endbound);
+		F = startbound;
+		htmlout = "<table class='table'>";
+		htmlout = htmlout + "<tr><th>Celsius</th><th>Fahrenheit</th></tr>"
+		do {
+			htmlout = htmlout + "<tr>"
+			C = getC(F);
+			console.log(C, F);
+			htmlout = htmlout + "<td>" + C.toFixed(2) + "</td>"
+			htmlout = htmlout + "<td>" + F.toString() + "</td>"
+			F = F + increment;
+			htmlout = htmlout + "</tr>"
+		} while (F < endbound)
+		htmlout = htmlout + "</table>"
+		$('#details').html(htmlout);
+	});
+});
